7b984357fe9674a659a5bae2769fc1758cd42a09
huteikei <andoromedazadazo11@gmail.com>
2025-07-31 02:17:02 +0900

    デバッグモードの取り消し

--- Assets/Scripts/Result/EndingController.cs
+++ Assets/Scripts/Result/EndingController.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using UnityEditor.ShaderKeywordFilter;
-
 public class EndingController : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer spriteRenderer;
